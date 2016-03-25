8e15c768129efa789920776958321aad4c875013
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2016-03-25 22:34:01 +0100

    Completing first example

---
+++ examples/00-divs-with-color.js
@@ -0,0 +1,19 @@
+var datas = [ { value: 100, color: "red" },
+ { value: 300, color: "green" },
+ { value: 400, color: "red" },
+ { value: 200, color: "blue" },
+ { value: 500, color: "red" }
+];
+
+d3.select(".container")
+    .selectAll(".value")
+      .data(datas)
+      .enter()
+        .append("div")
+        .attr("class", "value")
+        .style({
+          "background-color": x => x.color,
+          "height" : "20px",
+          "margin" : "10px",
+          width: x => x.value + "px"
+        });
