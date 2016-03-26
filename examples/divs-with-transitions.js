e863e9e4747353a8658547167c842114c950be53
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2016-03-26 11:43:48 +0100

    Adding complete data changing example

---
+++ examples/divs-with-transitions.js
@@ -0,0 +1,39 @@
+function render(someDatas) {
+  var selection = d3.select(".container")
+      .selectAll(".value")
+        .data(someDatas);
+
+    selection.enter()
+          .append("div")
+          .attr("class", "value")
+          .style({
+              "background-color": "blue",
+              "height" : "20px",
+              "margin" : "10px"
+            });
+
+    doTransition(selection.exit())
+              .style("background-color", "red")
+              .transition()
+              .duration(500)
+              .style("width", "0px")
+              .remove();
+
+    doTransition(selection)
+          .style({
+              width: x => x + "px"
+            });
+}
+
+function doTransition(selection) {
+  return selection.transition()
+  .duration(1000);
+}
+
+var datas = [100, 300, 400, 200, 500];
+
+render(datas);
+
+var newDatas = [300, 100, 400];
+
+setTimeout(() => render(newDatas), 1000);
