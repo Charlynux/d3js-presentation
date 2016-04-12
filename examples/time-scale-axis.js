c7b3e2fa8e6d7f3c668f28b1cce022a768188f6a
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2016-04-12 18:44:59 +0200

    Update time examples

--- examples/time-scale-axis.js
+++ examples/time-scale-axis.js
@@ -1,41 +1,50 @@
-d3.json('datas/projects.json', function(error, data) {
-    render(data);
+d3.json("datas/projects.json", (err, json) => {
+  if (err) console.error(err);
+
+  json.forEach(d => {
+    d.start = new Date(d.start);
+    d.end = new Date(d.end);
+  });
+
+  render(json)
 });
 
-function render(datas) {
-  var min = d3.min(datas, d => d.start);
-  var max = d3.max(datas, d => d.end);
+
+function render(data) {
+  var min = d3.min(data, d => d.start);
+  var max = d3.max(data, d => d.end);
+
+  var xScale = d3.time.scale()
+        .domain([min, max])
+        .range([0, 500]);
+
+  var axis = d3.svg.axis()
+                .scale(xScale)
+                .ticks(d3.time.months, 3);
 
   var svg = d3.select('.container')
   .append('svg')
     .attr({
       width: 500,
-      height: 300
+      height: 800
     });
 
-    var scale = d3.time.scale()
-        .domain([new Date(min), new Date(max)])
-        .range([0, 400]);
-
-  var axis = d3.svg.axis()
-                    .ticks(d3.time.months, 3)
-                    .orient("bottom")
-                    .scale(scale);
-
   svg.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0, 200)")
-        .call(axis);
+        .attr("class", "axis")
+        .attr("transform", "translate(0, 200)")
+        .call(axis)
+            .selectAll("text")
+            .style("text-anchor", "start")
+            .attr("transform", "rotate(45)");
 
   svg.selectAll("rect")
-      .data(datas)
-      .enter()
-        .append("rect")
-        .attr({
-          height: 10,
-          fill: "blue",
-          x: d => scale(new Date(d.start)),
-          y: (d, i) => i * 20 + 5,
-          width: (d, i) => scale(new Date(d.end)) - scale(new Date(d.start))
-        });
+          .data(data).enter()
+          .append("rect")
+          .attr({
+            x : d => xScale(d.start) + 5,
+            y: (d, i) => (i * 25) + 5,
+            width: d => xScale(d.end) - xScale(d.start),
+            height: 10,
+            fill: "blue"
+          })
 }
--- examples/time-scale.js
+++ examples/time-scale.js
@@ -1,28 +1,37 @@
 d3.json("datas/projects.json", (err, json) => {
   if (err) console.error(err);
+
+  json.forEach(d => {
+    d.start = new Date(d.start);
+    d.end = new Date(d.end);
+  });
+
   render(json)
 });
 
 
 function render(data) {
-  var xScale = d3.time.scale()
-        .domain([new Date("2015-08-15"), new Date("2016-12-13")])
-        .range([0, 400]);
+  var min = d3.min(data, d => d.start.getTime());
+  var max = d3.max(data, d => d.end.getTime());
 
-  d3.select(".container")
-      .append("svg")
-        .attr({
-          width: 400,
-          height: 600
-        })
-        .selectAll("rect")
-          .data(data)
-          .enter()
+  var xScale = d3.time.scale()
+        .domain([min, max])
+        .range([0, 500]);
+
+  var svg = d3.select('.container')
+  .append('svg')
+    .attr({
+      width: 500,
+      height: 800
+    });
+
+  svg.selectAll("rect")
+          .data(data).enter()
           .append("rect")
           .attr({
-            x : d => xScale(new Date(d.start)) + 5,
+            x : d => xScale(d.start) + 5,
             y: (d, i) => (i * 25) + 5,
-            width: d => xScale(new Date(d.end)) - xScale(new Date(d.start)),
+            width: d => xScale(d.end) - xScale(d.start),
             height: 10,
             fill: "blue"
           })
